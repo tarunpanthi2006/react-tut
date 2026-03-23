6a8c565fc7fe4b8d5fcad426d31430217f9716cd
tarunpanthi2006 <tarunplaystation4@gmail.com>
2026-03-23 19:04:00 +0530

    first commit

---
+++ 01-folder_reactJS_tut/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>HEYY</h1>
+      <h3>mai hu tarun</h3>
+      <h5>tu kaun hai bey</h5>
+    </div>
+  )
+}
+
+export default App
---
+++ script.js
@@ -0,0 +1,9 @@
+var h1=document.createElement('h1')
+
+h1.innerHTML = 'Hello from JS'
+
+document.body.appendChild(h1)
+
+import a from '/app.js'
+
+console.log(a)
