6950120550b32427cd56c77ea4e588e87936cde9
tarunpanthi2006 <tarunplaystation4@gmail.com>
2026-03-23 19:13:55 +0530

    first time

---
+++ folder-2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>About my friend SUJAL</h1>
+      <h2>Sujal dalla tha</h2>
+      <h3>Sujal dalla hai</h3>
+      <h4>Sujal dalla rahega</h4>
+      <h5>kayenaat tak Sujal dalla rahega</h5>
+    </div>
+  )
+}
+
+export default App
